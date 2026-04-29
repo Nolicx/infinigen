911d2b422576f240c2e4ad701f0a3d5a56011189
sthimm <s.thimm@hs-mannheim.de>
2026-04-29 09:54:34 +0200

    Add blender files via LFS

--- or_generator/configs/floor_plans/angio_room.py
+++ or_generator/configs/floor_plans/angio_room.py
@@ -3,7 +3,19 @@ from infinigen.core.constraints.example_solver.room.base import room_name
 from infinigen.core.tags import Semantics
 
 
-def angio_room_floorplan(seed):
+def angio_room_simple(seed):
+    """
+    Simple rebuild of angiography room at UMM
+
+    Dimensions:
+
+    Wall thickness = 0.15
+
+    x1: 0
+    x2: 9.75 + 2*0.15
+    y1: 0
+    y2: 6.0 + 2*0.15
+    """
     return {
         "rooms": {
             room_name(Semantics.Utility, 0): {
